c4e2a434c677828f29a231c0b3f370b60d9f099c
Harim Choi <harim1206@gmail.com>
2018-02-08 20:17:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,8 +27,8 @@ def assign_rooms(names)
   arr
 end
 
-def printer
-  batch_badge_creator
-  assign_rooms
+def printer(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
 
 end
