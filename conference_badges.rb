95578c2f907882125478c77436996bef731f98f5
Harim Choi <harim1206@gmail.com>
2018-02-08 19:54:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+
+  arr.each do |name|
+    badge_maker(name)
+  end
+
+end
+
+def assign_rooms(speakersArr)
+
+#rooms 1-7
+  puts "Hello #{speaker}! You will be assigned to room #{room number}!"
+
+
+
+end
